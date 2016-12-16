69a595d7d30a2f66ee7c5c0682927cc0c582b67c
Thom Allen <thomwork@gmail.com>
2016-12-16 01:14:35 +0000

    Add v basic spawning of random role creep when energy buildup occurs

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var sp1 = Game.spawns.Spawn1;
+
+var roles = [
+  'harvester',
+  'upgrader',
+  'builder',
+];
+var nextRole = 0;
 
 module.exports.loop = function () {
+  
+  var zeroOneTwo = Math.round(Math.random() * 2);
+  var randomRole = roles[zeroOneTwo];
 
+    if (sp1.energy >= 200) {
+      sp1.createCreep([WORK, CARRY, MOVE], null, {role: randomRole});
+      console.log('spawning', randomRole);
+    }
+    
+    console.log(randomRole);
+    
     var tower = Game.getObjectById('TOWER_ID');
     if(tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
