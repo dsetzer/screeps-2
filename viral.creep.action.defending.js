929db5cb5c20753b249b33fa2c1cd3768bfaae90
Thom Allen <thomwork@gmail.com>
2017-03-02 21:40:15 +0000

    Rm log from viral.action.defending

--- viral.creep.action.defending.js
+++ viral.creep.action.defending.js
@@ -2,7 +2,6 @@
 const mod = {
   isValidTarget: function(target){
     const validity = (target && target.hits != null && target.hits > 0 && target.my == false && target.owner.username !== 'Source Keeper' );
-    console.log('sk detected, not valid trg', target, validity);
     return validity;  
   },
   newTarget: function(creep){
